485f83bfaf0214a38f1d882056037ec713a53f30
Lucas Berlanda <lucasberlanda02@gmail.com>
2019-11-05 00:01:29 -0300

    lista de requisições abertas/pendentes

--- Tcc2/sistema/app/routes/requisicaoPecas.py
+++ Tcc2/sistema/app/routes/requisicaoPecas.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, redirect, request, flash
 from app import app, db
-from app.models import Peca, TipoBomba, Requisicao
+from app.models import Peca, TipoBomba, Requisicao, Usuario
 from app.forms import RequisicaoForm
 from flask_login import login_required, current_user
 
@@ -39,3 +39,20 @@ def requisicao():
             flash('Um erro aconteceu. Tente novamente!', 'error')
 
     return render_template('requisicao/novo.html', title='Requisição', form=form)
+
+
+@app.route('/requisicoes/abertas', methods=['GET', 'POST'])
+@login_required
+def requisicoesAbertas():
+
+    users = Usuario.query.all()
+    page = request.args.get('page', 1, type=int)
+    requisicoes = Requisicao.query.order_by(Requisicao.equipamento).paginate(page, app.config['POSTS_PER_PAGE'], False)
+
+    next_url = url_for('requisicoesAbertas', page=requisicoes.next_num) \
+        if requisicoes.has_next else None
+    prev_url = url_for('requisicoesAbertas', page=requisicoes.prev_num) \
+        if requisicoes.has_prev else None
+
+    return render_template("requisicao/abertas.html", lista=requisicoes.items, next_url=next_url, prev_url=prev_url,
+                           title='Requisições abertas', users=users)
